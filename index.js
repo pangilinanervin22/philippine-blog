74c4d6b787f23b9176eac3c93a01cf5937e24dd4
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-05-18 00:02:07 +0800

    aos added and fix disaster

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const list = ["Kamusta Ka", "Magandang Araw", "Salamat Sayo "];
 
 let index = 0;
 
-setTimeout(() => {
-	setInterval(() => {
-		title.innerHTML = " ";
-		const next = list[index++ % 3];
-		let indexChart = 0;
-		const typeEveryLetter = setInterval(() => {
-			if (indexChart >= next.length) return clearInterval(typeEveryLetter);
+// setTimeout(() => {
+// 	setInterval(() => {
+// 		title.innerHTML = " ";
+// 		const next = list[index++ % 3];
+// 		let indexChart = 0;
+// 		const typeEveryLetter = setInterval(() => {
+// 			if (indexChart >= next.length) return clearInterval(typeEveryLetter);
 
-			title.innerHTML += next[indexChart++];
-		}, 100);
-	}, 3000);
-}, 5000);
+// 			title.innerHTML += next[indexChart++];
+// 		}, 100);
+// 	}, 3000);
+// }, 5000);
 
 const menuButton = document.getElementsByClassName("menu")[0];
 const dropDown = document.getElementsByClassName("nav_dropdown")[0];
